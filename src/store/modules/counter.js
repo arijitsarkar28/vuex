711f333cd3fd9fcf868566a463360a04ab9cd12a
Arijit <tarijit.1900@gmail.com>
2020-07-26 03:23:56 +0530

    changes file structures. moved actions, mutations, getters to new files

---
+++ src/store/modules/counter.js
@@ -0,0 +1,48 @@
+import * as types from '../types';
+
+const state = {
+    counter: 0
+};
+const getters = {
+    [types.DOUBLE_COUNTER]: state => {
+        return state.counter * 2;
+    },
+    [types.CLICK_COUNTER]: state => {
+        return state.counter + ' clicks';
+    }
+};
+
+const mutations = {
+    increment: (state, payload = 1) => {
+        state.counter += payload;
+    },
+    decrement: (state, payload) => {
+        state.counter -= payload;
+    }
+};
+
+const actions = {
+    increment: (context, payload) => {
+        context.commit('increment', payload);
+    },
+    decrement: (context, payload) => {
+        context.commit('decrement', payload);
+    },
+    asyncIncrement: ({commit}, payload) => {
+        setTimeout(() => {
+            commit('increment', payload.by)
+        }, payload.duration);
+    },
+    asyncDecrement: ({commit}, payload) => {
+        setTimeout(() => {
+            commit('decrement', payload.by)
+        }, payload.duration);
+    }
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
